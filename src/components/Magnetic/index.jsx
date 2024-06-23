8eee74c3ce943abe7b2950b3e2e92d2189423981
Mahesh Paul <123456magimagi@gmail.com>
2024-06-23 11:32:02 +0530

    window not found fixed

--- src/components/Magnetic/index.jsx
+++ src/components/Magnetic/index.jsx
@@ -1,43 +1,42 @@
-import React, { useEffect, useRef } from 'react'
+"use client";
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
-const Magnetic = ({children}) => {
+const Magnetic = ({ children }) => {
     const magnetic = useRef(null);
-    const isMobileOrTablet = useRef(window.innerWidth <= 768);
 
     useEffect(() => {
-        const handleResize = () => {
-            isMobileOrTablet.current = window.innerWidth <= 768;
-        };
+        // Check if the code is running on the client side
+        if (typeof window !== 'undefined') {
+            const isMobileOrTablet = window.innerWidth <= 768;
 
-        window.addEventListener('resize', handleResize);
+            if (!isMobileOrTablet) {
+                const xTo = gsap.quickTo(magnetic.current, "x", { duration: 1, ease: "elastic.out(1, 0.3)" });
+                const yTo = gsap.quickTo(magnetic.current, "y", { duration: 1, ease: "elastic.out(1, 0.3)" });
 
-        if (!isMobileOrTablet.current) {
-            const xTo = gsap.quickTo(magnetic.current, "x", { duration: 1, ease: "elastic.out(1, 0.3)" });
-            const yTo = gsap.quickTo(magnetic.current, "y", { duration: 1, ease: "elastic.out(1, 0.3)" });
+                const handleMouseMove = (e) => {
+                    const { clientX, clientY } = e;
+                    const { height, width, left, top } = magnetic.current.getBoundingClientRect();
+                    const x = clientX - (left + width / 2);
+                    const y = clientY - (top + height / 2);
+                    xTo(x * 0.35);
+                    yTo(y * 0.35);
+                };
 
-            const handleMouseMove = (e) => {
-                const { clientX, clientY } = e;
-                const { height, width, left, top } = magnetic.current.getBoundingClientRect();
-                const x = clientX - (left + width / 2);
-                const y = clientY - (top + height / 2);
-                xTo(x * 0.35);
-                yTo(y * 0.35);
-            };
+                const handleMouseLeave = () => {
+                    xTo(0);
+                    yTo(0);
+                };
 
-            const handleMouseLeave = () => {
-                xTo(0);
-                yTo(0);
-            };
+                magnetic.current.addEventListener("mousemove", handleMouseMove);
+                magnetic.current.addEventListener("mouseleave", handleMouseLeave);
 
-            magnetic.current.addEventListener("mousemove", handleMouseMove);
-            magnetic.current.addEventListener("mouseleave", handleMouseLeave);
-
-            return () => {
-                window.removeEventListener('resize', handleResize);
-                magnetic.current.removeEventListener("mousemove", handleMouseMove);
-                magnetic.current.removeEventListener("mouseleave", handleMouseLeave);
-            };
+                // Cleanup event listeners on component unmount
+                return () => {
+                    magnetic.current.removeEventListener("mousemove", handleMouseMove);
+                    magnetic.current.removeEventListener("mouseleave", handleMouseLeave);
+                };
+            }
         }
     }, []);
 
